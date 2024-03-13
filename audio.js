07bb5b7656e151e522109f52ba4a9bc9b995a3bc
toobajaved <toobajaved1@hotmail.com>
2024-03-13 14:05:07 -0300

    Added audio.js file

---
+++ audio.js
@@ -0,0 +1,75 @@
+
+function speakInput(button) {
+    var inputBox = button.parentNode;
+    var input = inputBox.querySelector('input[type="text"], input[type="date"]');
+    var inputText = input.value.trim();
+
+    if (inputText === '') {
+        speak("You have not entered any input.");
+    } else {
+        speak(inputText);
+    }
+
+    // Check if the input box is part of the gender details section
+    if (inputBox.closest('.gender-details')) {
+        var genderInput = inputBox.querySelector('input[type="radio"]');
+        if (genderInput && genderInput.checked) {
+            speakGender(genderInput);
+        }
+    }
+}
+
+function speakQuestion(button) {
+    var question = button.parentNode.querySelector('.details').textContent;
+    speak(question);
+}
+
+function speakOption(select) {
+    var selectedOption = select.options[select.selectedIndex].text;
+    speak(selectedOption);
+}
+
+function speakSelectedOption(button) {
+    var select = button.previousElementSibling;
+    var selectedOption = select.options[select.selectedIndex].text;
+    if (selectedOption !== "") {
+        speak(selectedOption);
+    } else {
+        speak("Please select your gender.");
+    }
+}
+
+function speakGender(input) {
+    var gender = input.value;
+    speak(gender);
+    if (gender !== "Other") {
+        document.getElementById('otherGenderInput').style.display = 'none';
+    }
+}
+
+function showGenderInput() {
+    var genderSelect = document.getElementById('gender');
+    var selectedGender = genderSelect.options[genderSelect.selectedIndex].value;
+    if (selectedGender === "other") {
+        document.getElementById('otherGenderInput').style.display = 'block';
+    } else {
+        document.getElementById('otherGenderInput').style.display = 'none';
+    }
+}
+
+function speakGenderInput() {
+    var genderInput = document.getElementById('otherGender');
+    var inputText = genderInput.value.trim();
+    if (inputText === '') {
+        speak("You have not entered any input.");
+    } else {
+        speak(inputText);
+    }
+    
+}
+
+function speak(text) {
+    var synth = window.speechSynthesis;
+    var utterance = new SpeechSynthesisUtterance(text);
+    synth.speak(utterance);
+}
