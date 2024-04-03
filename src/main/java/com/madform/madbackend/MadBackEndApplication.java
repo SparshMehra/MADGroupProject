014e00719dada0ab5015e6cd7a04aa94644513d1
maximusscorp <zaid.zk467@gmail.com>
2024-04-03 15:26:37 -0300

    Added Comments

--- src/main/java/com/madform/madbackend/MadBackEndApplication.java
+++ src/main/java/com/madform/madbackend/MadBackEndApplication.java
@@ -3,6 +3,8 @@ package com.madform.madbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This is the main class of the backend.
+// It is the entry point of the application.
 @SpringBootApplication
 public class MadBackEndApplication {
 
