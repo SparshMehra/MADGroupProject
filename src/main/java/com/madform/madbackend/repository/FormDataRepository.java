3dfd167b98343e2763a4efbf52a545d62894f6e8
maximusscorp <zaid.zk467@gmail.com>
2024-04-03 15:26:49 -0300

    Added Comments

--- src/main/java/com/madform/madbackend/repository/FormDataRepository.java
+++ src/main/java/com/madform/madbackend/repository/FormDataRepository.java
@@ -4,6 +4,8 @@ import com.madform.madbackend.model.FormData;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// Repository is an interface that extends CrudRepository which is used to
+// perform CRUD(Create, Read, Update, Delete) operations on the database
 @Repository
 public interface FormDataRepository extends CrudRepository<FormData, Long> {
 }
